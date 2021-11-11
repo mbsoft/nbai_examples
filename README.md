ce99b5d939f35b0853575b9b8072e33458ca31c0
Jim Welch <jim.welch@nextbillion.ai>
2021-11-11 06:19:53 -0500

    refactors and route comparison tool

---
+++ README.md
@@ -0,0 +1,48 @@
+# API documentation
+[https://doc.maps.nextbillion.io/api_reference/]
+
+# Directions
+![Alt text](./screen_compare.png "Screenshot")
+## Description
+Utility used to perform route result comparisons (distance/duration) across different service providers.
+- random set of lat/lon pairs within a defined area-of-interest
+- define number of routes to compare
+- output is comparison of results from NBAI, TomTom, Google, and Mapbox
+
+## Quick Start
+- `npm install`
+- modify the `.env` file for
+  - entries API_HOST (http://localhost:9999 or https://api.nextbillion.io) 
+  - API_KEY (your assigned key)
+  - AREA_OF_INTEREST (based on geojson files in `data` folder - atlanta, bangalore, la, london, newyork, ohio, ontario, sf, south yorkshire, texas currently)
+  - keys required for other service providers - TomTom, Google, and Mapbox
+- alter the number of routes to be compared in the constant defined in `directions/compare.js`
+- `node directions/compare.js --aoi la`
+
+# Distance Matrix
+![Alt text](./screenshot.png "Screenshot")
+## Description
+Javascript/node.js example of Nextbillion.ai travel/distance matrix calculation API using Flatbuffer, Protobuf, and JSON endpoints. 
+- random set of lat/lon pairs within a defined polygon
+- default size of grid is 8 x 8
+- `.env` file is read to determine endpoint, key, area of interest
+- command line parameters allow user to set area-of-interest, nuber of origins, and number of destinations
+
+## Quick Start
+- install Flatbuffers `brew install flatbuffer`
+- install Protobuf `brew install protobuf`
+- install TypeScript transpiler `brew install tsc`
+- compile the nextbillion.ai schema to generate TypeScript files `flatc --ts nbai_fbschema.fbs`
+- compile the protobuf definition to JavaScript `protoc --proto_path=. --js_out=library=nbai_pb/matrix,binary:build/gen  nbai_protos.proto`
+- `npm install`
+- transpile TS to JS `tsc build/gen/nbai_fb/*.ts`
+- modify the `.env` file for 
+  - entries API_HOST (http://localhost:9999 or https://api.nextbillion.io) 
+  - API_KEY (your assigned key)
+  - AREA_OF_INTEREST (based on geojson files in `data` folder - la, london, ontario, south yorkshire currently)
+- Usage: `node distancematrix/distmatrix_json.json --origins {int} --destinations {int} --aoi atlanta|bangalore|dallas|la|london|newyork|ohio|ontario|southyorkshire`
+- output to console is the time distance grid and the size of the response
+
+
+## Questions
+jim.welch@nextbillion.ai
---
+++ directions/directions.js
@@ -0,0 +1,79 @@
+var randomPointsOnPolygon = require('random-points-on-polygon');
+const logo = require('../common/logo');
+const colorize = require('../common/utils');
+var axios = require('axios');
+var polyline = require('@mapbox/polyline');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+var randomPointsOnPolygon = require('random-points-on-polygon');
+
+var poly = require(`../data/${process.env.AREA_OF_INTEREST}_poly.json`);
+
+const numberOfPoints = 1;
+const precision = 4;
+
+var maneuvers = [];
+
+logo();
+async function run() {
+
+    // Generate random points within the defined polygon
+    var points_origins = randomPointsOnPolygon(numberOfPoints, poly.features[0]);
+    var points_destinations = randomPointsOnPolygon(numberOfPoints, poly.features[0]);
+
+    var destArray = [];
+    var originArray = [];
+    var steps = [];
+
+
+    for (var j = 0; j < numberOfPoints; j++) {
+        destArray.push(points_destinations[j].geometry.coordinates[1].toFixed(precision) + ',' + points_destinations[j].geometry.coordinates[0].toFixed(precision));
+        originArray.push(points_origins[j].geometry.coordinates[1].toFixed(precision) + ',' + points_origins[j].geometry.coordinates[0].toFixed(precision));
+    }
+
+    let departureTime = Math.round(new Date().getTime()/1000);
+    await axios.get(`${process.env.API_HOST}/directions/json?key=${process.env.API_KEY}&annotations=true&steps=true&alternatives=false&origin=${originArray[0]}&destination=${destArray[0]}&mode=4w&departure_time=${departureTime}`)
+    .then((res) => {
+        var idx = 0;
+        res.data.routes.forEach(function(route) {
+            const route_points = polyline.decode(route.geometry);
+            //process.stdout.write(colorize(93,polyline.decode(route.geometry)));
+            console.log(`Distance  = ${(route.distance/1000.0).toFixed(2)}km`);
+            console.log(`Duration  = ${route.duration}sec`);
+            process.stdout.write('\n');
+            steps = route.legs[0].steps;
+
+        });
+
+    }).catch((err) => {
+        console.log(err);
+    });
+    var step = 1;
+    for (var i=0; i< steps.length; i++) {
+        await reverseGeocode(i, [`${steps[i].start_location.latitude},${steps[i].start_location.longitude}`]);
+    }
+    for (var i=0;i< steps.length; i++) {
+        console.log(`${maneuvers[i].index}-${maneuvers[i].address} ${JSON.stringify(steps[i].start_location)}`)
+        //maneuvers[i].start = steps[i].distance;
+    }
+    console.log('Done');
+}
+
+async function reverseGeocode(index, theArray) {
+    await axios.get(`https://api.nextbillion.io/h/revgeocode?key=${process.env.GEOCODE_API_KEY}&at=${theArray[0]}`)
+    .then((res) => {
+        maneuvers[index] = {
+            'index': index,
+            'address': res.data.items[0].address.label
+        }
+        //console.log(`${index} ${res.data.items[0].address.label}`);
+    }).catch((err) => {
+        console.log(err);
+    })
+}
+
+
+run().catch(err => console.log(err));
+
